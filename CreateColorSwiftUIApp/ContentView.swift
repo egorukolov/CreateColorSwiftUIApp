e0efc64e2530f0068935800eef7cda6cb3ff2336
Egor Ukolov <ukolovegor@yandex.ru>
2020-07-24 20:57:30 +0700

    Initial Commit

---
+++ CreateColorSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreateColorSwiftUIApp
+//
+//  Created by Egor Ukolov on 24.07.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
